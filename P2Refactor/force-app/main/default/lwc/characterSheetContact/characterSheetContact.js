fc1bd71c7e713349f2714c737173b00d9da194f5
Alex Walhout <alexpwalhout@gmail.com>
2020-06-16 15:37:23 -0400

    quick added a space for the init function in the JS

--- P2Refactor/force-app/main/default/lwc/characterSheetContact/characterSheetContact.js
+++ P2Refactor/force-app/main/default/lwc/characterSheetContact/characterSheetContact.js
@@ -3,4 +3,9 @@ import { LightningElement, api } from 'lwc';
 export default class CharacterSheetContact extends LightningElement {
     
     @api contactList;//array type from the aura attribute
+
+    //LWC init method
+    connectedCallback(){
+
+    }
 }
