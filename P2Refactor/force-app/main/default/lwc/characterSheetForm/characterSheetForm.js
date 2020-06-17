12079d5314d04ee208a8e6e4bb4175a4a0566af7
Alex Walhout <alexpwalhout@gmail.com>
2020-06-17 16:12:59 -0400

    working on figuring out how to do this and how to further separate the components

--- P2Refactor/force-app/main/default/lwc/characterSheetForm/characterSheetForm.js
+++ P2Refactor/force-app/main/default/lwc/characterSheetForm/characterSheetForm.js
@@ -1,3 +1,35 @@
 import { LightningElement } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Name';
+import CLASS__C_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Class__c';
+import STRENGTH__C_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Strength__c';
+import DEXTERITY__C_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Dexterity__c';
+import CONSTITUTION__C_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Constitution__c';
+import INTELLIGENCE__C_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Intelligence__c';
+import WISDOM__C_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Wisdom__c';
+import CHARISMA__C_FIELD from '@salesforce/schema/Character_Stat_Sheet__c.Charisma__c';
 
-export default class CharacterSheetForm extends LightningElement {}
+export default class CharacterSheetForm extends LightningElement {
+
+    //variables for field assignment
+    sheetObjectFields = [NAME_FIELD, 
+                         CLASS__C_FIELD, 
+                         STRENGTH__C_FIELD,
+                         DEXTERITY__C_FIELD,
+                         CONSTITUTION__C_FIELD,
+                         INTELLIGENCE__C_FIELD,
+                         WISDOM__C_FIELD,
+                         CHARISMA__C_FIELD]; 
+    characterName;
+    characterClass;
+    characterLevel;
+    characterStr;
+    characterDex;
+    characterCon;
+    characterInt;
+    characterWis;
+    characterCha;
+    characterInsp;
+
+    //holds the array of classes from the api
+    apiClassList;
+}
