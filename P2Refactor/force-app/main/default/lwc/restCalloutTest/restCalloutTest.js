5b9cc4d09ac8d0b03478485da36686bfb070992a
Alex Walhout <alexpwalhout@gmail.com>
2020-06-18 16:33:03 -0400

    threw together some brief stuff for it, now need to make a quick harness app for the test

--- P2Refactor/force-app/main/default/lwc/restCalloutTest/restCalloutTest.js
+++ P2Refactor/force-app/main/default/lwc/restCalloutTest/restCalloutTest.js
@@ -1,23 +1,26 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class RestCalloutTest extends LightningElement {
     //on init
+    @api jsonDisplay;
     connectedCallback(){
          //use fetch for client side callout
-         fetch('http://www.dnd5eapi.co/api/classes/', 
+         fetch('http://www.dnd5eapi.co/api/classes/cleric/', 
          {method:"GET"})
          .then((response) => {return response.json();})
          .then((jsonResponse) => {
              let objData = {
                  //setup an object to bind json vars
-                 count : undefined,
-                 results : undefined
+                 name : '',
+                 hit_die : undefined
              };
+
+             jsonDisplay = JSON.stringify(jsonResponse);
 
              console.log('jsonResponse: ' + JSON.stringify(jsonResponse));
 
-             objData.count = jsonResponse.count;
-             objData.results = jsonResponse.results;
+             /*objData.name = jsonResponse.name;
+             objData.hit_die = jsonResponse.hit_die;*/
          });//see more in the link
          ///https://www.salesforcecodecrack.com/2019/08/make-rest-api-callout-in-lwc.html
     }
