1e8e83a6b0232c761afbe204595bb4aeefd75ccf
Alex Walhout <alexpwalhout@gmail.com>
2020-06-18 10:11:05 -0400

    put together enough that i really need to test, so hooking the LWC into the aura to see what's broken

--- P2Refactor/force-app/main/default/lwc/characterSheetForm/characterSheetForm.js
+++ P2Refactor/force-app/main/default/lwc/characterSheetForm/characterSheetForm.js
@@ -43,8 +43,15 @@ export default class CharacterSheetForm extends LightningElement {
             .then((response) => {return response.json();})
             .then((jsonResponse) => {
                 let objData = {
-                    //bind json vars?
+                    //setup an object to bind json vars
+                    count : undefined,
+                    results : undefined
                 };
+
+                console.log('jsonResponse: ' + JSON.stringify(jsonResponse));
+
+                objData.count = jsonResponse.count;
+                objData.results = jsonResponse.results;
             });//see more in the link
             ///https://www.salesforcecodecrack.com/2019/08/make-rest-api-callout-in-lwc.html
         //create the action
