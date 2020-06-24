0b19fb78953964505abd0660350b9068090a60ef
Alex Walhout <alexpwalhout@gmail.com>
2020-06-24 11:57:28 -0400

    added the changes to the challenge component

--- workingWithDataInLWC/force-app/main/default/lwc/contactList/contactList.js
+++ workingWithDataInLWC/force-app/main/default/lwc/contactList/contactList.js
@@ -1,4 +1,5 @@
 import { LightningElement, wire } from 'lwc';
+import { reduceErrors } from 'c/ldsUtils';
 
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
@@ -17,4 +18,9 @@ export default class ContactList extends LightningElement {
     
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
 }
